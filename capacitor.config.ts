16f65c97f22ac18fde95ecfb90a4433caf4eb48b
jaewon kim <jaewon705@yanadoocorp.com>
2023-02-06 12:29:48 +0900

    1.킷폴더추가 및 capaictor설정 추가

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'com.sungbok.acafe',
   appName: 'acafe',
   webDir: 'dist',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server:{
+    url : "http://localhost:3000", //<=이부분은 연결할 주소로 설정
+    cleartext : true
+  }
 };
 
 export default config;
