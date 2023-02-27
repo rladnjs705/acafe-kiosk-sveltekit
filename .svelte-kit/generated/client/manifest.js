4727ce9e443933c71a14529f477adf60affbf93b
jaewon705 <rladnjs705@naver.com>
2023-02-27 15:27:05 +0900

    1.폴더구조 변경 및 개인정보처리방침 추가

--- .svelte-kit/generated/client/manifest.js
+++ .svelte-kit/generated/client/manifest.js
@@ -6,16 +6,19 @@ export const nodes = [() => import('./nodes/0'),
 	() => import('./nodes/3'),
 	() => import('./nodes/4'),
 	() => import('./nodes/5'),
-	() => import('./nodes/6')];
+	() => import('./nodes/6'),
+	() => import('./nodes/7'),
+	() => import('./nodes/8')];
 
 export const server_loads = [];
 
 export const dictionary = {
-	"/": [2],
-	"/menu": [3],
-	"/users": [6],
-	"/user/join": [4],
-	"/user/login": [5]
+	"/(app)": [3,[2]],
+	"/(app)/menu": [4,[2]],
+	"/privacy": [8],
+	"/(app)/users": [7,[2]],
+	"/(app)/user/join": [5,[2]],
+	"/(app)/user/login": [6,[2]]
 };
 
 export const hooks = {
--- .svelte-kit/generated/client/nodes/2.js
+++ .svelte-kit/generated/client/nodes/2.js
@@ -1,1 +1,3 @@
-export { default as component } from "../../../../src/routes/+page.svelte";
+import * as universal from "../../../../src/routes/(app)/+layout.js";
+export { universal };
+export { default as component } from "../../../../src/routes/(app)/+layout.svelte";
