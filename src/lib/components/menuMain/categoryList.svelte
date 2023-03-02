e7d820f10cea08d9336fb2faa6f1e1c74a384e40
jaewon705 <rladnjs705@naver.com>
2023-03-02 17:19:59 +0900

    1.카테고리 다시 수정

--- src/lib/components/menuMain/categoryList.svelte
+++ src/lib/components/menuMain/categoryList.svelte
@@ -1,18 +1,6 @@
 <script lang="ts">
   import { modalActiveCategory, categoryList, itemCategorySelected, itemList, itemPageLock, isAdmin, itemMainLoading } from '$stores';
   import { ALL } from '$utils/constans';
-  import { onMount } from "svelte";
-
-  let isMobile = false;
-
-  onMount(() => {
-    checkMobile();
-    window.addEventListener('resize', checkMobile);
-  })
-
-  function checkMobile() {
-    isMobile = window.innerWidth < 600;
-  }
 
   $: data = $categoryList; 
 
@@ -26,7 +14,6 @@
       }
     };
 </script>
-{#if !isMobile}
 <div class="header-box-bottom">
   <ul class="d-flex justify-content-start">
     <li>
@@ -57,44 +44,3 @@
     {/if}                                                                       
   </ul>
 </div>
-  {:else}
-<div class="header-box-bottom mobile">
-  <ul class="d-flex justify-content-start">
-    <li>
-      {#if $itemCategorySelected === ALL}
-        <a href="#null" class="d-flex align-items-center" class:selected={$itemCategorySelected===ALL} >모두보기</a>
-      {:else}
-        <a href="#null" class="d-flex align-items-center" class:disableClick={$itemMainLoading} on:click={() => onSelectCategory(ALL)} >모두보기</a>
-      {/if}
-    </li>
-    {#if data}
-      {#each data.list as category(category.categoryId)}
-      <li>
-        {#if $itemCategorySelected===category.categoryId}
-          <a  href="#null" class="d-flex align-items-center" class:selected={$itemCategorySelected===category.categoryId}>{category.categoryName}</a>
-        {:else}
-          <a  href="#null" class="d-flex align-items-center" class:disableClick={$itemMainLoading} on:click={() => onSelectCategory(category.categoryId)}>{category.categoryName}</a>
-        {/if}
-      </li>  
-      {/each}
-      {:else}
-        <p class="d-flex align-items-center">Loading</p>
-    {/if}
-
-    {#if $isAdmin}
-      <li>
-        <a href="#null" class="d-flex align-items-center add" on:click={onOpenModalActiveCateogry} >+관리</a>
-      </li>         
-    {/if}                                                                       
-  </ul>
-</div>
-{/if}
-
-<style lang="scss">
-  .mobile ul li a {
-      height: 7vh !important;
-      padding: 0 10px !important;
-      font-size: 1rem !important;
-      text-decoration: none;
-  }
-</style>
