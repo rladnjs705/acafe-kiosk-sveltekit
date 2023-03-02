4aef3933356486f09aa18f70614625fdf3249490
jaewon705 <jaewon705@yanadoocorp.com>
2023-03-02 10:12:00 +0900

    1.확대 방지

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,14 +2,37 @@
 	import '$lib/styles/bootstrap/bootstrap.min.css';
 	import '$lib/styles/boxicon/css/boxicons.min.css';
 	import '$lib/styles/main.scss';
+    import { onMount } from 'svelte';
+
+	let isMobile = false;
+
+	onMount(() => {
+		checkMobile();
+		window.addEventListener('resize', checkMobile);
+	})
+
+	function checkMobile() {
+ 		isMobile = window.innerWidth < 600;
+	}
+
 </script>
 <svelte:head>
-	<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1, user-scalable=yes,initial-scale=1.0" />
+	{#if isMobile}
+		<meta name="viewport" content="width=device-width, maximum-scale=5.0, minimum-scale=0.1, user-scalable=yes,initial-scale=0.7" />
+		{:else}
+		<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1, user-scalable=yes,initial-scale=1.0" />
+	{/if}
   	<title>아카페</title>
 </svelte:head>
 
 <body>
-	<div class="app d-flex container-fluid ">
-		<slot />
-	</div>
+	{#if isMobile}
+		<div class="app">
+			<slot />
+		</div>
+		{:else}
+		<div class="app d-flex container-fluid ">
+			<slot />
+		</div>
+	{/if}
 </body>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -5,7 +5,7 @@
 <svelte:head>
 	<title>아카페</title>
 	<meta name="description" content="acafe demo app" />
-	<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1, user-scalable=yes,initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 </svelte:head>
 
 <LoginForm />
